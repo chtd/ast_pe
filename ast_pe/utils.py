33e99e7b7462bd19ced7baca17757b5347d97c52
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2012-09-08 10:51:47 +0400

    comparing ASTs is enough for tests

--- ast_pe/utils.py
+++ ast_pe/utils.py
@@ -5,15 +5,21 @@ import inspect
 
 
 def get_ast(fn):
-    ''' Return ast tree, parsed from fn
+    ''' Return AST tree, parsed from fn
     '''
     return ast.parse(inspect.getsource(fn))
 
 
 def eval_ast(tree):
-    ''' Evaluate ast tree, which sould contain only one root node
+    ''' Evaluate AST tree, which sould contain only one root node
     '''
     assert isinstance(tree, ast.Module) and len(tree.body) == 1
     code_object = compile(tree, '<nofile>', 'exec')
     eval(code_object)
     return locals()[tree.body[0].name]
+
+
+def ast_equal(tree1, tree2):
+    ''' Returns whether AST tree1 is equal to tree2 '''
+    return ast.dump(tree1) == ast.dump(tree2)
+
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 
 import ast
+from ast import Module, FunctionDef, arguments, Name, Param, If, Compare, \
+        Return, BinOp, Load, Add, Subscript, Index, Str, Eq
 import unittest
 
 import ast_pe.utils
-import ast_pe.astpretty
 
 
 class TestCase(unittest.TestCase):
@@ -19,23 +20,51 @@ class TestCase(unittest.TestCase):
                 "[Return(BinOp(Name('x', Load()), Add(), Name('y', Load())))], "
                 "[Return(Subscript(Name('kw', Load()), "
                 "Index(Str('zzz')), Load()))])], [])])")
+
+    def test_compare_ast(self):
+        tree = ast_pe.utils.get_ast(sample_fn)
+        expected_tree = Module([FunctionDef('sample_fn', 
+            arguments([Name('x', Param()), Name('y', Param()), 
+                Name('foo', Param())], None, 'kw', [Str('bar')]), 
+            [If(Compare(Name('foo', Load()), [Eq()], [Str('bar')]), 
+                [Return(BinOp(Name('x', Load()), Add(), Name('y', Load())))], 
+                [Return(Subscript(Name('kw', Load()), 
+                    Index(Str('zzz')), Load()))])], [])])
+        self.assertTrue(ast_pe.utils.ast_equal(tree, expected_tree))
+        self.assertFalse(ast_pe.utils.ast_equal(
+            tree, ast_pe.utils.get_ast(sample_fn2)))
+        self.assertFalse(ast_pe.utils.ast_equal(
+            tree, ast_pe.utils.get_ast(sample_fn3)))
+
+    def test_compile_ast(self):
+        tree = ast_pe.utils.get_ast(sample_fn)
         compiled_fn = ast_pe.utils.eval_ast(tree)
         self.assertEqual(compiled_fn(3, -9), sample_fn(3, -9))
         self.assertEqual(
                 compiled_fn(3, -9, 'z', zzz=map), 
                 sample_fn(3, -9, 'z', zzz=map))
     
-    def test_ast_pp(self):
-        tree = ast_pe.utils.get_ast(sample_fn)
-        s = ast_pe.astpretty.ast_to_string(tree)
-        print s
-
 
 def sample_fn(x, y, foo='bar', **kw):
     if foo == 'bar':
         return x + y
     else:
         return kw['zzz']
+
+
+def sample_fn2(x, y, foo='bar', **kw):
+    if foo == 'bar':
+        return x - y
+    else:
+        return kw['zzz']
+
+
+def sample_fn3(x, y, foo='bar', **kwargs):
+    if foo == 'bar':
+        return x + y
+    else:
+        return kwargs['zzz']
+
 
 if __name__ == '__main__':
     unittest.main()
